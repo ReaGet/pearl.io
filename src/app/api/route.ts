0d21743f142f7e3e425136376edd310d7d9d269f
Rifat Mukhametzyanov <rifat@avtoalfa.com>
2024-10-18 16:51:47 +0300

    some work

---
+++ src/app/api/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server'
+import puppeteer from 'puppeteer'
+import { v4 as uuid4 } from 'uuid'
+ 
+export async function GET() {
+  const browser = await puppeteer.launch()
+  const imageName = uuid4()
+  try {
+    const page = await browser.newPage();
+    await page.goto("https://avtoalfa.com");
+    await page.screenshot({ path: `public/screenshots/${imageName}.png` });
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await browser.close();
+  }
+  return NextResponse.json({
+    hello: `/screenshots/${imageName}.png`
+  })
+}
