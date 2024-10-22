f53f1a0363e691e71270c522205dfb3bb5e816b6
Rifat Mukhametzyanov <rifat@avtoalfa.com>
2024-10-22 14:09:04 +0300

    feat: add clerk auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,30 @@
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api(.*)', '/'])
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect()
+  }
+})
  
-export function middleware(request: NextRequest) {
-  const sourceUrl = request.nextUrl
+// export function middleware(request: NextRequest) {
+//   const sourceUrl = request.nextUrl
 
-  // if (sourceUrl.pathname === '/api/get')
-  //   return NextResponse.redirect(new URL('/get', request.url))
+//   // if (sourceUrl.pathname === '/api/get')
+//   //   return NextResponse.redirect(new URL('/get', request.url))
 
-  if (sourceUrl.pathname === '/get')
-    return NextResponse.rewrite(new URL('/api/get', request.url))
+//   if (sourceUrl.pathname === '/get')
+//     return NextResponse.rewrite(new URL('/api/get', request.url))
 
-  return NextResponse.next()
-}
+//   return NextResponse.next()
+// }
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
 }
