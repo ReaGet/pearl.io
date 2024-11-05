1c14b2ed0cb0b3c3908a66fec45c3a5679033dbd
Rifat Mukhametzyanov <rifat@avtoalfa.com>
2024-11-05 16:29:25 +0300

    feat: add lib to use installed browser with puppeeter

--- src/lib/screenshot.ts
+++ src/lib/screenshot.ts
@@ -1,4 +1,5 @@
 'use server'
+import chromium from '@sparticuz/chromium'
 import puppeteer, { Page } from 'puppeteer'
 import { v4 as uuid4 } from 'uuid'
 import { imageSavingPath } from './folders'
@@ -36,7 +37,12 @@ export const screenshotViewportBlob = async (url: string, delay: number = 0): Pr
 type GetPuppeteerPageParam = (page: Page) => Promise<any>
 
 export const getPuppeteerPage = async (url: string, callback: GetPuppeteerPageParam) => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
+  })
   try {
     const page = await browser.newPage()
 
